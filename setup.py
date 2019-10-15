13ee34f47b0ec7917d0d4449c40d55f40249c50f
Liu <lupl@amazon.com>
2019-10-15 00:04:49 -0700

    Setup.py to enable building

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="snapshotalyzer-30000",
+    version='0.1',
+    author="pengliu89",
+    description="SnapshotAlyzer 30000 is a tool to manage AWS EC2 snapshots",
+    license="GPLv3+",
+    packages=['shotty'],
+    url="https://github.com/pengliu89/snapshotalyzer-30000",
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+    ''',
+
+)
